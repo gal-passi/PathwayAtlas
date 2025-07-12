84e5cd0cc146fbef52f275e53a2e3e3c60309a71
Gal Passi <galpassi+1@gmail.com>
2025-07-12 21:08:19 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,1 @@
-from definitions import DIRS_TO_CREATE
 
-def setup_directories():
-    for path in DIRS_TO_CREATE:
-        os.makedirs(path, exist_ok=True)
-
-if __name__ == "__main__":
-    setup_directories()
