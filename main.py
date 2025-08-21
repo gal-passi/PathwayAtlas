8084f61aa1f4efd772c3e6317247c5662a22d0a4
lotemsenderov <lotem.senderov@mail.huji.ac.il>
2025-08-21 13:09:31 +0300

    moved cbio code to new file cbio.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from Kegg import *
 from esm import pretrained      # for model choosing and alphabet
 
 
-
 def init_kegg_genome(recalc=False):
     """Initialize KEGG genome by creating KeggGene objects for all genes in KEGG database."""
     # TODO maybe create a small dict from kegg_id to sequence, and save in in the data folder, for future use.
@@ -105,53 +104,6 @@ def create_llr_scoring(kegg: KeggApi):
             print(f"Error processing {kegg_id}: {e}")
 
 
-def check_for_duplicates(dfs):
-    """Check for duplicate mutations in all the studies.
-        @param dfs: dictionary of study id to df of that study"""
-    all_patients = []
-    for study, df in dfs.items():
-        patients = df['PatientId'].unique()
-        all_patients.extend(patients)
-
-    all_df = pd.DataFrame(all_patients, columns=['PatientId', 'StudyId'])
-
-    overlaps = (all_df.groupby('PatientId')['StudyId']
-                .nunique()
-                .reset_index()
-                .query("StudyId > 1")
-                )
-
-    if overlaps.empty:
-        print("No overlapping patients found.")
-    else:
-        print(f"found {len(overlaps)} overlapping patients.")
-
-
 if __name__ == '__main__':
     # Lab Notebook:
-    #    https://docs.google.com/document/d/1XR21LBpqW3q96BjExqsbH6JgEhV3Yc9sJuzG3abzUmY/edit?usp=sharing
-
-    cbio = CbioApi()
-    dfs = {}
-    all_studies = cbio.api.Studies.getAllStudiesUsingGET().result()
-
-    for study in all_studies:
-        study_id = study.studyId
-        try:
-            # download mutations
-            results = cbio.download_study_mutations(study_id)
-
-            # define file name for each study
-            outpath = f"data/cbio/studies/{study_id}_mutations.csv"
-
-            if os.path.exists(outpath):
-                print(f"{study_id} already downloaded.")
-            else:
-                # save
-                df = cbio.study_to_csv(results, outpath=outpath)
-                dfs[study_id] = df
-                print(f"Saved {outpath}")
-        except Exception as e:
-            print(f"Skipping {study_id}: {e}")
-
-    check_for_duplicates(dfs)
+    # https://docs.google.com/document/d/1XR21LBpqW3q96BjExqsbH6JgEhV3Yc9sJuzG3abzUmY/edit?usp=sharing
