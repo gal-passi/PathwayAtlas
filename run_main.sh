12e632122872b60497c26c1cf789ff253d5f1292
ophirmil12 <ophirmil12@gmail.com>
2025-08-25 14:27:52 +0300

    conda libs env...

--- run_main.sh
+++ run_main.sh
@@ -9,17 +9,27 @@
 #SBATCH -o slurm.out.%A_%a.out
 
 
+
+# IMPORTANT: Replace /cs/labs/dina/ophirmil12/miniforge3 with the actual path to your miniforge3 installation
+# This initializes Conda in the current shell session.
+source /cs/labs/dina/ophirmil12/miniforge3/etc/profile.d/conda.sh
+
+# Activate your specific Conda environment
+conda activate project_env
+
+
+
 # go to PathwayAtlas folder [local git folder - cd .]
 cd /cs/labs/dina/ophirmil12/PathwayAtlas/
 
-# startup venv
-source ./venv/bin/activate
+## startup venv       NO NEED WHEN HAVE CONDA
+#source ./venv/bin/activate
 
 # install all requirements
 # pip install --no-cache-dir -r requirements.txt
 
 # export esm model path to local venv
-export TORCH_HOME=./venv/torch_cache
+export TORCH_HOME=./torch_cache
 
 # run code (output in slurm .out file)
 python -u main.py
