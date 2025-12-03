a7fd6a39a855fc9e591bb8a0974c349c8aedce6a
lotemsenderov <lotem.senderov@mail.huji.ac.il>
2025-12-03 14:24:33 +0200

    updated the permutation testing to work on all distance metrics

--- run_permutation.sh
+++ run_permutation.sh
@@ -10,10 +10,10 @@
 ###########SBATCH --gres=gg:g4:1
 
 DISTANCE_METRIC="kl_divergence"
-CANCER_DATA_DIR="/cs/labs/dina/ophirmil12/PathwayAtlas/results_and_graphs/scores/clinvar_reg_dis_ordered_prob-" + $DISTANCE_METRIC
+CANCER_SCORES_DIR="/cs/labs/dina/ophirmil12/PathwayAtlas/results_and_graphs/scores/clinvar_reg_dis_ordered_prob-" + $DISTANCE_METRIC
 
 # Get all CSV files into an array
-mapfile -t FILES < <(find "$CANCER_DATA_DIR" -type f -name "*.csv")
+mapfile -t FILES < <(find "$CANCER_SCORES_DIR" -type f -name "*.csv")
 
 NUM_CANCER_FILES=${#FILES[@]}
 
@@ -23,9 +23,11 @@ if [ "$NUM_CANCER_FILES" -eq 0 ]; then
 fi
 
 # FILE corresponding to this array task ID
-CANCER_FILE="${FILES[$SLURM_ARRAY_TASK_ID]}"
+CANCER_SCORES_FILE="${FILES[$SLURM_ARRAY_TASK_ID]}"
 
-echo "Processing file: $CANCER_FILE"
+echo "Processing file: $CANCER_SCORES_FILE"
 
 # Run your Python script with the file as parameter
-python main.py --cancer_file "$CANCER_FILE" "$DISTANCE_METRIC"
+python main.py --cancer_file "CANCER_SCORES_FILE" "$DISTANCE_METRIC"
+
+echo "Job completed for file: $CANCER_SCORES_FILE"
